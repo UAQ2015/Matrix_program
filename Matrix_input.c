30e966c351d990588ea261aadcb5047aa8a246ed
Pepe <pepe@debian>
2015-09-11 22:18:42 -0500

    converting code intoa function

--- Matrix_input.c
+++ Matrix_input.c
@@ -2,6 +2,13 @@
 
 int main()
 {
+    int n=3;
+    int A[n][n];
+    A=insert();
+    return 0;
+}
+int insert()
+{
     int n=3,i,j;
     int A[n][n];
     for (i=0;i<n;i++)
@@ -10,6 +17,6 @@ int main()
             printf("\nInput value %i, %i: ", 1+i,1+j);
             scanf("%i",&A[i][j]);
             }
-    return 0;
+    return A;
 }
 
