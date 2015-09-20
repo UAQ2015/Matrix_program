141d87961a4f717314a5faad085ab725f8102c7e
gyivan <gyivan@hotmail.com>
2015-09-20 14:12:07 -0500

    menu try 1

--- Matrix_input.c
+++ Matrix_input.c
@@ -3,16 +3,81 @@
 void multiplication(int size,int matrixres[size][size],int matrix1[size][size],int matrix2[size][size]);
 int determinant(int size, int matrix[size][size]);
 void insert(int size,int matrix[size][size]);
+void insert_null(int size,int matrix[size][size]);
 void add( int size, int matrix1[size][size],int matrix2[size][size],int matrix3[size][size]);
 void substract( int size, int matrix1[size][size],int matrix2[size][size],int matrix3[size][size]);
 void transpose (int size, int matrix[size][size], int transposed_matrix[size][size]);
 void print_matrix (int size, int matrix [size][size]);
+void menu (void);
 int main()
 {
-    int size=3;
-    int matrix[size][size];
-    insert(size,matrix);
+    menu;	
     return 0;
+}
+void menu ()
+{
+	int option=0, result;
+	int size=3;
+    	int matrix1[size][size];
+	int matrix2[size][size];
+	int matrix3[size][size];
+
+    	insert(size,matrix);
+	do
+	{
+	do
+	{
+		printf("Click the number of what do you want to do\n");
+		printf("1.- Add\n2.- Substract\n3.- Multiplication\n4.- Determinant\n5.- Transpose\n6.- Inverse\n7.- Exit ");
+		scanf("%i", &option);
+		if(option>7||option==0)
+		{
+			printf("Incorrect number, try again");
+			option=0;
+		}	
+	}while(option==0);
+	switch(option)
+		case 1:
+			insert(size,matrix1[size][size]);
+			insert(size,matrix2[size][size]);
+			insert_null(size,matrix3[size][size]);
+			add(size,matrix1[size][size],matrix2[size][size],matrix3[size][size]);
+			print_matrix(size, matrix3[size][size]);
+			break;
+		case 2:
+			insert(size,matrix1[size][size]);
+                        insert(size,matrix2[size][size]);
+                        insert_null(size,matrix3[size][size]);
+                        substract(size,matrix1[size][size],matrix2[size][size],matrix3[size][size]);
+                        print_matrix(size, matrix3[size][size]);
+                        break;
+		case 3:
+			insert(size,matrix1[size][size]);
+                        insert(size,matrix2[size][size]);
+                        insert_null(size,matrix3[size][size]);
+                        multiplication(size,matrix3[size][size],matrix1[size][size],matrix2[size][size]);
+                        print_matrix(size, matrix3[size][size]);
+                        break;
+		case 4:
+			insert(size,matrix1[size][size]);
+			result=determinant(size,matrix1);
+			break;
+		case 5:
+						
+			insert(size,matrix1[size][size]);
+                        insert_null(size,matrix2[size][size]);
+			transpose(matrix1[size][size],matrix2[size][size];
+			print_matrix(size,matrix2[size][size]);
+			break; 
+		case 6:
+			break;
+		case 7:
+			break;
+	system("clear"); 
+	}while(option!=7);
+}
+			
+			
 }
 void insert(int size,int matrix[size][size])
 {
@@ -24,6 +89,14 @@ void insert(int size,int matrix[size][size])
             scanf("%i",&matrix[row][col]);
         }
 }
+void insert_null(int size,int matrix[size][size])
+{
+    int row,col;
+    for (row=0;row<size;row++)
+        for (col=0;col<size;col++)
+            matrix[row][col]=0;
+}
+
 void multiplication(int size,int matrixres[size][size],int matrix1[size][size],int matrix2[size][size])
 {
     int row,col,aux;
