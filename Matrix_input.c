87a66840671626f31a56666e166f024a1101edcd
gyivan <gyivan@hotmail.com>
2015-10-01 20:48:02 -0500

    Fatal error

--- Matrix_input.c
+++ Matrix_input.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <math.h>
-void multiplication(int size1, int size2, int size3, int **matrixres,int **matrix1,int **matrix2);
-int determinant(int size, int **matrix);
-void scalar_multiplication(int size1, int size2, int **matrix1, int **matrix_result,int scalar);
-void inverse_matrix (int size, int **matrix, int **inverse_matrix);
-void insert(int size1, int size2, int **matrix);
-void add( int size1, int size2, int **matrix1,int **matrix2,int **matrix3);
-void substract(int size1, int size2, int **matrix1,int **matrix2,int **matrix3);
-void transpose (int size1, int size2, int **matrix, int **transposed_matrix);
-void print_matrix (int size1, int size2, int **matrix);
+void multiplication(int size1, int size2, int size3, float **matrixres,float **matrix1,float **matrix2);
+float determinant(int size, float **matrix);
+void scalar_multiplication(int size1, int size2, float **matrix1, float **matrix_result,float scalar);
+void inverse_matrix (int size, float **matrix, float **inverse_matrix);
+void insert(int size1, int size2, float **matrix);
+void add( int size1, int size2, float **matrix1,float **matrix2,float **matrix3);
+void substract(int size1, int size2, float **matrix1,float **matrix2,float **matrix3);
+void transpose (int size1, int size2, float **matrix, float **transposed_matrix);
+void print_matrix (int size1, int size2, float **matrix);
 void menu (void);
-void insert_null(int size1, int size2 ,int **matrix);
-int **calloc_matrix( int size1, int size2);
-void free_matrix(int **matrix, int size1);
+void insert_null(int size1, int size2 ,float **matrix);
+float **calloc_matrix( int size1, int size2);
+void free_matrix(float **matrix, int size1);
 int main()
 {
 	menu();	
@@ -21,10 +21,10 @@ int main()
 
 void menu ()
 {
-	int option=9, result;
+	int option=9;
 	int size1, size2, size3; 
-	int scalar;
-	int **matrix1,**matrix2,**matrix3;
+	float result,scalar;
+	float **matrix1,**matrix2,**matrix3;
 	do
 	{
 		do
@@ -109,7 +109,7 @@ void menu ()
 				printf("\nGive me the matrix\n");
 				insert(size1,size2,matrix1);
 				printf("Give the scalar\n");
-				scanf("%i", &scalar);
+				scanf("%d", &scalar);
 				insert_null(size1,size2,matrix3);
 				scalar_multiplication(size1,size2,matrix1,matrix3,scalar);
 				print_matrix(size1,size2, matrix3);
@@ -123,7 +123,7 @@ void menu ()
 				printf("\nGive me the matrix\n");
 				insert(size1,size1,matrix1);
 				result=determinant(size1,matrix1);
-				printf("\nYour determinant is: %i\n", result);
+				printf("\nYour determinant is: %d\n", result);
 				free_matrix(matrix1,size1);
 				break;
 			case 6:
@@ -166,23 +166,23 @@ void menu ()
 		} 
 	}while(option!=0);
 }
-int **calloc_matrix(int size1, int size2)
+float **calloc_matrix(int size1, int size2)
 {
 	int i=0;
-	int **matrix;
+	float **matrix;
 	matrix=calloc(sizeof(int *),size1);
 	for(i=0; i<size1; i++)
-		matrix[i]=calloc(sizeof(int),size2);
+		matrix[i]=calloc(sizeof(double),size2);
 	return matrix;
 }
-void free_matrix(int **matrix, int size1)
+void free_matrix(float **matrix, int size1)
 {
 	int i=0;
 	for(i=0; i<size1; i++)
 		free(matrix[i]);
 	free(matrix);
 }
-void scalar_multiplication(int size1, int size2, int **matrix1, int **matrix_result,int scalar)
+void scalar_multiplication(int size1, int size2, float **matrix1, float **matrix_result,float scalar)
 {
 	int row,col;
         for (row=0;row<size1;row++)
@@ -190,17 +190,17 @@ void scalar_multiplication(int size1, int size2, int **matrix1, int **matrix_res
 			matrix_result[row][col]=scalar*matrix1[row][col];
 }	
 
-void insert(int size1, int size2, int **matrix)
+void insert(int size1, int size2, float **matrix)
 {
 	int row,col;
 	for (row=0;row<size1;row++)
 		for (col=0;col<size2;col++)
 		{
 			printf("\nInput value in row  %i and column  %i: ", 1+row,1+col);
-			scanf("%i",&matrix[row][col]);
+			scanf("%f",&matrix[row][col]);
 		}
 }
-void insert_null(int size1, int size2 ,int **matrix)
+void insert_null(int size1, int size2 ,float **matrix)
 {
 	int row,col;
 	for (row=0;row<size1;row++)
@@ -208,7 +208,7 @@ void insert_null(int size1, int size2 ,int **matrix)
 			matrix[row][col]=0;
 }
 
-void multiplication(int size1, int size2, int size3, int **matrixres,int **matrix1,int **matrix2)
+void multiplication(int size1, int size2, int size3, float **matrixres,float **matrix1,float **matrix2)
 {
 	int row,col,aux;
 	for(row=0;row<size1;row++)
@@ -216,14 +216,15 @@ void multiplication(int size1, int size2, int size3, int **matrixres,int **matri
 			for(aux=0,matrixres[row][col]=0;aux<size2;aux++)
 				matrixres[row][col]=matrixres[row][col]+matrix1[row][aux]*matrix2[aux][col];
 }
-int determinant(int size, int **matrix)
+float determinant(int size, float **matrix)
 {
-	int result=0,i,j,h,pow;
+	float result=0;
+	int i,j,h,pow;
 	if (size==1)
 		return matrix[0][0];
 	else
 	{
-		int **submatrix;
+		float **submatrix;
 		submatrix=calloc_matrix(size-1,size-1);
 		for(i=0;i<size;i++)
 		{
@@ -244,7 +245,7 @@ int determinant(int size, int **matrix)
 		return result;
 	}
 }
-void add( int size1, int size2, int **matrix1,int **matrix2,int **matrix3)
+void add( int size1, int size2, float **matrix1,float **matrix2,float **matrix3)
 {
 	int row, col;
 	for (row=0;row<size1;row++)
@@ -254,7 +255,7 @@ void add( int size1, int size2, int **matrix1,int **matrix2,int **matrix3)
 		}
 }
 
-void substract(int size1, int size2, int **matrix1,int **matrix2,int **matrix3)
+void substract(int size1, int size2, float **matrix1,float **matrix2,float **matrix3)
 {
 	int row, col;
 	for (row=0;row<size1;row++)
@@ -265,7 +266,7 @@ void substract(int size1, int size2, int **matrix1,int **matrix2,int **matrix3)
 }
 
 
-void transpose (int size1, int size2, int **matrix, int **transposed_matrix)
+void transpose (int size1, int size2, float **matrix, float **transposed_matrix)
 {
 	int row, col;
 	for (row = 0; row < size1; row++)
@@ -273,21 +274,21 @@ void transpose (int size1, int size2, int **matrix, int **transposed_matrix)
 			transposed_matrix[col][row] = matrix[row][col];
 }
 
-void print_matrix (int size1, int size2, int **matrix)
+void print_matrix (int size1, int size2, float **matrix)
 {
 	int row,col;
 	printf("\nYour result is:\n");
 	for (row=0;row<size1;row++)
 	{    
 		for (col=0;col<size2;col++)   
-			printf(" %i   ", matrix[row][col]);
+			printf(" %d   ", matrix[row][col]);
 		printf("\n");
 	}    
 }
 
-void inverse_matrix (int size, int **matrix, int **inverse_matrix)
+void inverse_matrix (int size, float **matrix, float **inverse_matrix)
 {
-	int det;
+	float det;
 	det = (1/ determinant(size, matrix));
 	inverse_matrix [0][0] = det * ((matrix [1][1] * matrix [2][2]) - (matrix [1][2] * matrix [2][1]));
 	inverse_matrix [0][1] = det * ((matrix [2][1] * matrix [0][2]) - (matrix [2][2] * matrix [0][1]));	
